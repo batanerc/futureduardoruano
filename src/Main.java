36eb539269d68e8c062f285f212960da13dc8bf4
Eduardo <batanerc@gmail.com>
2022-11-21 23:07:54 +0100

    Finalización del Proyecto

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Estudiante " + args[0] + " : " );
+        System.out.println(veredicto(args[1], args[2]));
+
+    }
+
+    public static String veredicto(String valor1, String valor2) {
+
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+
+        if (Integer.parseInt(valor1) < Integer.parseInt(valor2)) {
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+
+        return "Ideal. Trabajas los contenidos en casa.";
+
+    }
+
+}
